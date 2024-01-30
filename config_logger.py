aec9097dbd123b13aa0243bad2948b156de2b2da
ANDREI NOVOZHILOV <sitrun@users.noreply.github.com>
2024-01-30 13:40:34 +0700

    delete deprecated async crypto, add logs for send

--- config_logger.py
+++ config_logger.py
@@ -5,11 +5,28 @@ from logging import StreamHandler, Formatter
 from logging.handlers import TimedRotatingFileHandler
 
 logger = logging.getLogger('logger')
+
+
 logger.setLevel(logging.INFO)
 handler_stdout = StreamHandler(stream=sys.stdout)
 handler_fileout = TimedRotatingFileHandler(filename=os.path.join("logs", "main_logs.log"), when='D', interval=1,
                                            backupCount=14, encoding='utf-8')
+
+
 handler_stdout.setFormatter(Formatter(fmt='[%(asctime)s: %(levelname)s] %(message)s'))
 handler_fileout.setFormatter(Formatter(fmt='[%(asctime)s: %(levelname)s] %(message)s'))
 logger.addHandler(handler_stdout)
 logger.addHandler(handler_fileout)
+
+# # # # # ЛОГИРОВАНИЕ ОТПРАВКИ
+# Отправленные с ошибкой
+log_send_fails = logging.getLogger('logger')
+log_send_fails.setLevel(logging.INFO)
+
+# Тем кому не отправлять
+log_send_no_send = logging.getLogger('logger')
+log_send_no_send.setLevel(logging.INFO)
+
+# Список отправки
+log_send_no_send = logging.getLogger('logger')
+log_send_no_send.setLevel(logging.INFO)
--- initialize.py
+++ initialize.py
@@ -16,24 +16,6 @@ from TariffManager import TariffManager
 from TextEditor import TextEditor
 
 
-crypto = AioCryptoPay(token=cryptopay_token, network=Networks.MAIN_NET)
-
-async def start_cryptopay():
-    profile = await crypto.get_me()
-    # currencies = await crypto.get_currencies()
-    balance = await crypto.get_balance()
-    rates = await crypto.get_exchange_rates()
-    # print(profile, currencies, balance, rates, sep='\n')
-    logger.info(f'-----> Криптобот удачно запустился profile:')
-    logger.info(profile)
-    logger.info(f'-----> Криптобот баланс:')
-    logger.info(balance)
-
-    async def close_session() -> None:
-        await crypto.close()
-
-asyncio.run(start_cryptopay())
-
 state_storage = StateMemoryStorage()
 bot = TeleBot(
     TOKEN_MAIN_BOT, 'HTML',
