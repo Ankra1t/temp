730ecb8c1d255c3e10c9673183b970f612a80a8f
Азат Файзрахманов <azat_fayzrahmanov@mail.ru>
2024-03-29 22:25:27 +0300

    fix

--- MAIN/commands.py
+++ MAIN/commands.py
@@ -59,24 +59,25 @@ def _site(message: Message, bot: TeleBot):
 
 def _test(message: Message, bot: TeleBot):
     # CHAT_KEY = -1002104767484
-    def text_to_image(
-        text: str,
-        font_filepath: str,
-        font_size: int,
-    ) -> ImageType:
-        font = ImageFont.truetype(font_filepath, size=font_size)
+    # def text_to_image(
+    #     text: str,
+    #     font_filepath: str,
+    #     font_size: int,
+    # ) -> ImageType:
+    #     font = ImageFont.truetype(font_filepath, size=font_size)
 
-        img = Image.new("RGBA", font.getmask(text).size)
+    #     img = Image.new("RGBA", font.getmask(text).size)
 
-        draw = ImageDraw.Draw(img)
-        draw_point = (0, 0)
+    #     draw = ImageDraw.Draw(img)
+    #     draw_point = (0, 0)
 
-        draw.multiline_text(draw_point, text, font=font, fill=color)
+    #     draw.multiline_text(draw_point, text, font=font, fill=color)
 
-        text_window = img.getbbox()
-        img = img.crop(text_window)
+    #     text_window = img.getbbox()
+    #     img = img.crop(text_window)
 
-        return img
+    #     return img
+    pass
 
 
 def commands_registration(bot: TeleBot):
