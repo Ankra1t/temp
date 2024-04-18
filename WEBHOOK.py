e0f0b1b7e81784129635e3f544475f2501ddcd94
Азат Файзрахманов <azat_fayzrahmanov@mail.ru>
2024-04-18 08:59:36 +0300

    change WEBHOOK

--- WEBHOOK.py
+++ WEBHOOK.py
@@ -1,4 +1,5 @@
-from Classes.YooKassa import payment_updates
+from Classes.CryptoBot import cryptoPay_payment_updates
+from Classes.YooKassa import yooKassa_payment_updates
 import telebot
 import flask
 from flask import request
@@ -52,23 +53,22 @@ def BBB():
 # ======================= // ANCHOR WebHook Connect
 @app.route(base_url + CRYPTOPAY_URL, methods=['POST', 'GET'])
 def cryptobot_updates():
-    print(f'Cryptobot sent on {CRYPTOPAY_URL} request ')
-    print(request)
-    return pays.get_updates(request)
+    print(f'{CRYPTOPAY_URL} request')
+    return cryptoPay_payment_updates(request)
 
 
 # ======================= // ANCHOR WebHook Connect BITBANKER
-@app.route(base_url + BITBANKER_URL, methods=['POST', 'GET'])
-def bitbanker_updates():
-    print(f'BitBanker sent on {BITBANKER_URL} request ')
-    print(request)
-    return pays_banker.get_updates(request)
+# @app.route(base_url + BITBANKER_URL, methods=['POST', 'GET'])
+# def bitbanker_updates():
+#     print(f'BitBanker sent on {BITBANKER_URL} request ')
+#     print(request)
+#     return pays_banker.get_updates(request)
 
 
 @app.route(base_url + YOOKASSA_URL, methods=['POST', 'GET'])
 def yookassa_updates():
     print(f'{YOOKASSA_URL} request')
-    return payment_updates(request)
+    return yooKassa_payment_updates(request)
 
 
 app.run(host='127.0.0.1', port=flask_port)
