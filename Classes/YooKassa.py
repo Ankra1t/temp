36a380665ec1198ff9c12896b03652f61c51a8b2
Азат Файзрахманов <azat_fayzrahmanov@mail.ru>
2024-05-06 15:55:51 +0300

    add yoKassa param

--- Classes/YooKassa.py
+++ Classes/YooKassa.py
@@ -52,7 +52,8 @@ def yooKassa_create_payment(user_id: int, tariff: Price, redirect_url: str):
                 "amount": amount,
                 "quantity": str(tariff.duration_days),
                 "measure": "day",
-                "vat_code": 1
+                "vat_code": 1,
+                "payment_mode": "full_payment"
             }],
             "tax_system_code": 2
         }
