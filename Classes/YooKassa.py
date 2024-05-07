b2037d30ebdf3368ecbe8c4230ac1a2fceda01a4
Азат Файзрахманов <azat_fayzrahmanov@mail.ru>
2024-05-07 11:21:43 +0300

    try yooKassa

--- Classes/YooKassa.py
+++ Classes/YooKassa.py
@@ -1,7 +1,7 @@
 import traceback
 from telebot import TeleBot
 from flask import Request, Response
-from yookassa import Configuration, Payment
+from yookassa import Configuration, Payment, Settings
 from requests.exceptions import HTTPError
 import uuid
 
@@ -26,6 +26,10 @@ def yooKassa_create_payment(user_id: int, tariff: Price, redirect_url: str):
     price = check_discount_price(tariff)
     currency = tariff.currency
     name = tariff.name
+
+    me = Settings.get_account_settings()
+    print(me)
+    return False
 
     user_db_id = db.get_user_id_by_tg_id(user_id)
     if user_db_id == 0:
