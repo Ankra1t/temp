5f1ccdfe81b4782ae03ee7f0affdb0d133f6c0ad
Азат Файзрахманов <azat_fayzrahmanov@mail.ru>
2024-06-21 16:38:10 +0300

    fix

--- db.py
+++ db.py
@@ -1437,8 +1437,8 @@ class Database:
         """Установить значения для автозаполения пользователя"""
         market = self.get_user_current_market(user_id)
 
-        if market == 'crypto' and value != 'USDT':
-            return False
+        # if market == 'crypto' and value != 'USDT':
+        #     return False
 
         query = 'UPDATE tgcalc_user_settings SET base_currency = %s WHERE user_id = %s AND market = %s'
         params = (value, user_id, market)
