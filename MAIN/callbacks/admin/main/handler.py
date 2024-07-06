fe286d07ff10c528f783d1e1efabc765ea7eb3f8
Азат Файзрахманов <azat_fayzrahmanov@mail.ru>
2024-07-07 01:55:08 +0300

    fix

--- MAIN/callbacks/admin/main/handler.py
+++ MAIN/callbacks/admin/main/handler.py
@@ -79,7 +79,7 @@ def _handle_callback(call: CallbackQuery, bot: TeleBot):
             else:
                 tools[tool or ''] = 1
 
-            if stat.trading_style is None:
+            if stat.trading_style is None or stat.trading_style == '':
                 pass
             elif stat.trading_style in styles.keys():
                 styles[stat.trading_style] += 1
