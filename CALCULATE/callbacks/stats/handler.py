307ba493d53e92e1815d12e0da976e615edfd68c
Azat <azat_fayzrahmanov@mail.ru>
2024-09-04 12:31:02 +0300

    fix

--- CALCULATE/callbacks/stats/handler.py
+++ CALCULATE/callbacks/stats/handler.py
@@ -1383,17 +1383,16 @@ def edit_live_info(
                     now_changed = '⚡️ <b>НОВОЕ:</b>\n\n' if lang == 'ru' else '⚡️ <b>NEW:</b>\n\n'
                     now_changed += f'\n{current_msg}'.strip()
 
-                if date in msges:
-                    msges[date] += current_msg
+                if 'main' in msges:
+                    msges['main'] += current_msg
                 else:
-                    msges[date] = current_msg
+                    msges['main'] = current_msg
 
-            msg = ''
-            for key in msges:
-                msg += f'\n\n⚡️ <b>{key} | '
-                msg += 'LIVE-сделки' if lang == 'ru' else 'LIVE-deals'
-                msg += '</b>\n\n'
-                msg += msges[key].strip()
+            day = 31 - 19 + get_datetime_now().day + 1
+            msg = f'⚡️ <b>{day} ' + ('ДЕНЬ МАРАФОНА' if lang == 'ru' else 'DAY OF MARATHON')
+            msg += '</b>\n\n'
+            msg += msges['main'].strip()
+
 
             if msg != '':
                 msg += '\n\n<b>'
