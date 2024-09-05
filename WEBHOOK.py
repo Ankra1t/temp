df0212fcbb8842ecdca49bbd0bd7e4172c9190c3
Azat <azat_fayzrahmanov@mail.ru>
2024-09-05 15:16:18 +0300

    fix

--- WEBHOOK.py
+++ WEBHOOK.py
@@ -157,7 +157,7 @@ async def setup():
     logger.info('Starting up: removing old webhook')
     await bot.remove_webhook()
     logger.info('Starting up: setting webhook')
-    # await bot.set_webhook(url=f'https://profmarkets.ai/{base_url}/AAA')
+    await bot.set_webhook(url=f'https://profmarkets.ai/{base_url}/AAA')
 
     app = web.Application()
 
