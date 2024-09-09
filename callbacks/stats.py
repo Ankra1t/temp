939224dbabf2de295c1711a7504c22f5b27d949a
Azat <azat_fayzrahmanov@mail.ru>
2024-09-09 12:02:21 +0300

    f

--- callbacks/stats.py
+++ callbacks/stats.py
@@ -1192,12 +1192,10 @@ async def edit_channel_post(bot: AsyncTeleBot, calc_id: int, updateLive=True):
     send_data = channel_calc.getByCalc(calc_id)
     messages = channel_calc.getSentMessagesByCalc(calc_id)
 
-    logger.info(calc)
-    logger.info(send_data)
-    logger.info(messages)
     if messages is None or calc is None or send_data is None:
         return
 
+    logger.info(1)
     for chId_i, chId in enumerate(messages.chIds):
         link = ''
         if messages.messages:
