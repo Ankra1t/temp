0fa9be0827c3596e3caf4d8f393b3dd73de922ee
Azat <azat_fayzrahmanov@mail.ru>
2024-09-09 11:58:26 +0300

    fix

--- callbacks/stats.py
+++ callbacks/stats.py
@@ -1188,6 +1188,7 @@ async def send_week_stats(bot: AsyncTeleBot, calcId: int | None = None, is_new_w
 
 
 async def edit_channel_post(bot: AsyncTeleBot, calc_id: int, updateLive=True):
+    logger.info(1)
     calc = calculation.get(calc_id)
     send_data = channel_calc.getByCalc(calc_id)
     messages = channel_calc.getSentMessagesByCalc(calc_id)
