966c2761f87b2e81baba9fc8c730bc3860975e6d
Azat <azat_fayzrahmanov@mail.ru>
2024-09-10 16:30:10 +0300

    check

--- callbacks/stats.py
+++ callbacks/stats.py
@@ -1232,6 +1232,7 @@ async def edit_channel_post(
 ):
     weekMessages = channel_calc.getSentMessagesByCalc(calc.id)
 
+    print(send_data.messages)
     if send_data.messages is None:
         return
 
