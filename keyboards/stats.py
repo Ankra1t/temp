415a0c56617c8429b73a875b0afb0e5bf213cab9
Azat <azat_fayzrahmanov@mail.ru>
2024-10-23 17:38:54 +0300

    fix

--- keyboards/stats.py
+++ keyboards/stats.py
@@ -322,7 +322,7 @@ def kb_calc_result(lang: LANGUAGES_TYPE, user_db_id: int, calc: Calculation, isR
                 )
             )
 
-        if not (calc.ActiveCalc and not send_data) and (calc.status == 'WAIT' or calc.status == 'DEAL'):
+        if not isActiveCalcSub and (calc.status == 'WAIT' or calc.status == 'DEAL'):
             buttons.append(
                 getButton(
                     '⚡️ ' + texts[lang]['result'],
@@ -353,24 +353,24 @@ def kb_calc_result(lang: LANGUAGES_TYPE, user_db_id: int, calc: Calculation, isR
                 getButton('Выложить в каналах', 'send_to_channels', calc.id)
             )
 
-        # if isActiveCalcSub and calc.ActiveCalc is None and calc.status == 'WAIT':
-        #     keyboard.add(
-        #         getButton(
-        #             '⚡️ ' + texts[lang]['active'],
-        #             'active_calc_a', calc.id
-        #         )
-        #     )
-        # elif calc.ActiveCalc and (calc.status == 'WAIT' or calc.status == 'DEAL'):
-        #     keyboard.add(
-        #         getButton(
-        #             '⚡️ ' + texts[lang]['active_p'],
-        #             'active_calc', calc.id
-        #         ),
-        #         getButton(
-        #             '🔄',
-        #             'refresh', calc.id
-        #         )
-        #     )
+        if isActiveCalcSub and calc.ActiveCalc is None and calc.status == 'WAIT':
+            keyboard.add(
+                getButton(
+                    '⚡️ ' + texts[lang]['active'],
+                    'active_calc_a', calc.id
+                )
+            )
+        elif calc.ActiveCalc and (calc.status == 'WAIT' or calc.status == 'DEAL'):
+            keyboard.add(
+                getButton(
+                    '⚡️ ' + texts[lang]['active_p'],
+                    'active_calc', calc.id
+                ),
+                getButton(
+                    '🔄',
+                    'refresh', calc.id
+                )
+            )
 
     return keyboard
 
